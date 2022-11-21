cde5ca8100d11029194dc471fb0952ed61fa27f6
Hung Pham <truonghungit@gmail.com>
2022-11-21 16:28:04 +0700

    fix error

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const jsonServer = require("json-server");
-const fs = require("fs");
+
+// const fs = require("fs");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = 3200; // <== You can change the port
+// const port = process.env.PORT || 3000; // <== You can change the port
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-// Add custom routes before JSON Server router
+// // Add custom routes before JSON Server router
 server.get("/echo", (req, res) => {
-  let rawdata = fs.readFileSync("./db.json");
-  let student = JSON.parse(rawdata);
-  res.jsonp(student);
+  res.send("Server is running");
 });
 
 server.use(router);
 
-server.listen(port);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log("API running on http://localhost:", port);
+});
